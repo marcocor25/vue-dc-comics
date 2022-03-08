2070b87340770ed77de6aa9137becdd1cc4d530d
Marco Corallo <94033528+marcocor25@users.noreply.github.com>
2022-03-08 20:20:29 +0100

    Ciclo footer completato

--- src/components/MyFooter.vue
+++ src/components/MyFooter.vue
@@ -3,47 +3,37 @@
     <div class="container">
       <div class="list-col">
         <div class="list-wrapper">
+          <!-- LISTA 1 -->
           <ul>
-            <h2>dc comics</h2>
-            <li>Characters</li>
-            <li>Comics</li>
-            <li>Movie</li>
-            <li>TV</li>
-            <li>Games</li>
-            <li>Videos</li>
-            <li>News</li>
+            <h2>{{ title1 }}</h2>
+            <li v-for="(element, index) in list1" :key="index">
+              {{ element.text }}
+            </li>
           </ul>
-
+          <!-- LISTA 4 -->
           <ul>
-            <h2>shop</h2>
-            <li>Shop DC</li>
-            <li>Shop DC Collectibles</li>
+            <h2>{{ title4 }}</h2>
+            <li v-for="(element, index) in list4" :key="index">
+              {{ element.text }}
+            </li>
           </ul>
         </div>
+        <!-- LISTA 2 -->
         <ul>
-          <h2>dc</h2>
-          <li>Terms Of Use</li>
-          <li>Privacy Policy (New)</li>
-          <li>Ad Choices</li>
-          <li>Advertising</li>
-          <li>Jobs</li>
-          <li>Subscription</li>
-          <li>Talent Workshops</li>
-          <li>CPSC Certificates</li>
-          <li>Ratings</li>
-          <li>Shop Help</li>
-          <li>Contact Us</li>
+          <h2>{{ title2 }}</h2>
+          <li v-for="(element, index) in list2" :key="index">
+            {{ element.text }}
+          </li>
         </ul>
+        <!-- LISTA 3 -->
         <ul>
-          <h2>sites</h2>
-          <li>DC</li>
-          <li>MAD Magazine</li>
-          <li>DC Kids</li>
-          <li>DC Universe</li>
-          <li>DC Power Visa</li>
+          <h2>{{ title3 }}</h2>
+          <li v-for="(element, index) in list3" :key="index">
+            {{ element.text }}
+          </li>
         </ul>
       </div>
-
+      <!-- BACKGROUND LOGO DC -->
       <div class="logo-col"></div>
     </div>
   </div>
@@ -52,6 +42,97 @@
 <script>
 export default {
   name: "MyFooter",
+  data() {
+    return {
+      title1: "dc comics",
+      title2: "dc",
+      title3: "sites",
+      title4: "shop",
+      list1: [
+        {
+          text: "Characters",
+        },
+        {
+          text: "Comics",
+        },
+        {
+          text: "Movie",
+        },
+        {
+          text: "TV",
+        },
+        {
+          text: "Games",
+        },
+        {
+          text: "Videos",
+        },
+        {
+          text: "News",
+        },
+      ],
+      list2: [
+        {
+          text: "Terms Of Use",
+        },
+        {
+          text: "Privacy Policy (New)",
+        },
+        {
+          text: "Ad Choices",
+        },
+        {
+          text: "Advertising",
+        },
+        {
+          text: "Jobs",
+        },
+        {
+          text: "Subscription",
+        },
+        {
+          text: "Talent Workshops",
+        },
+        {
+          text: "CPSC Certificates",
+        },
+        {
+          text: "Ratings",
+        },
+        {
+          text: "Shop Help",
+        },
+        {
+          text: "Contact Us",
+        },
+      ],
+      list3: [
+        {
+          text: "DC",
+        },
+        {
+          text: "MAD Magazine",
+        },
+        {
+          text: "DC Kids",
+        },
+        {
+          text: "DC Universe",
+        },
+        {
+          text: "DC Power Visa",
+        },
+      ],
+      list4: [
+        {
+          text: "Shop DC",
+        },
+        {
+          text: "Shop DC collectibles",
+        },
+      ],
+    };
+  },
 };
 </script>
 
