84b6669b67019ac8734f1eb0580adb3baa875f3e
Marco Corallo <94033528+marcocor25@users.noreply.github.com>
2022-03-08 19:19:51 +0100

    Ciclo header completato

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -7,16 +7,9 @@
 
       <div class="nav-link">
         <ul>
-          <li><a href="#">characters</a></li>
-          <li><a href="#">comics</a></li>
-          <li><a href="#">movies</a></li>
-          <li><a href="#">tv</a></li>
-          <li><a href="#">games</a></li>
-          <li><a href="#">collectibles</a></li>
-          <li><a href="#">videos</a></li>
-          <li><a href="#">fans</a></li>
-          <li><a href="#">news</a></li>
-          <li><a href="#">shop</a></li>
+          <li v-for="(element, index) in link" :key="index">
+            <a :href="element.href">{{ element.text }}</a>
+          </li>
         </ul>
       </div>
     </div>
@@ -26,6 +19,52 @@
 <script>
 export default {
   name: "MyHeader",
+  data() {
+    return {
+      link: [
+        {
+          text: "characters",
+          href: "#",
+        },
+        {
+          text: "comics",
+          href: "#",
+        },
+        {
+          text: "movies",
+          href: "#",
+        },
+        {
+          text: "tv",
+          href: "#",
+        },
+        {
+          text: "games",
+          href: "#",
+        },
+        {
+          text: "collectibles",
+          href: "#",
+        },
+        {
+          text: "videos",
+          href: "#",
+        },
+        {
+          text: "fans",
+          href: "#",
+        },
+        {
+          text: "news",
+          href: "#",
+        },
+        {
+          text: "shop",
+          href: "#",
+        },
+      ],
+    };
+  },
 };
 </script>
 
@@ -51,6 +90,16 @@ export default {
       ul {
         display: flex;
         gap: 15px;
+      }
+
+      li {
+        line-height: 75px;
+        border-bottom: 5px solid transparent;
+        transition: 200ms linear;
+
+        &:hover {
+          border-bottom: 5px solid #0082f9;
+        }
       }
 
       a {
