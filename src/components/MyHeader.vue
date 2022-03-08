a4d387a456564931ff254d4f8c42339e0de76952
Marco Corallo <94033528+marcocor25@users.noreply.github.com>
2022-03-08 18:31:02 +0100

    .

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,22 +1,24 @@
 <template>
-  <div class="container">
-    <figure class="logo">
-      <img src="../assets/img/dc-logo.png" />
-    </figure>
+  <div class="container-fluid">
+    <div class="container">
+      <figure class="logo">
+        <img src="../assets/img/dc-logo.png" />
+      </figure>
 
-    <div class="nav-link">
-      <ul>
-        <li><a href="#">characters</a></li>
-        <li><a href="#">comics</a></li>
-        <li><a href="#">movies</a></li>
-        <li><a href="#">tv</a></li>
-        <li><a href="#">games</a></li>
-        <li><a href="#">collectibles</a></li>
-        <li><a href="#">videos</a></li>
-        <li><a href="#">fans</a></li>
-        <li><a href="#">news</a></li>
-        <li><a href="#">shop</a></li>
-      </ul>
+      <div class="nav-link">
+        <ul>
+          <li><a href="#">characters</a></li>
+          <li><a href="#">comics</a></li>
+          <li><a href="#">movies</a></li>
+          <li><a href="#">tv</a></li>
+          <li><a href="#">games</a></li>
+          <li><a href="#">collectibles</a></li>
+          <li><a href="#">videos</a></li>
+          <li><a href="#">fans</a></li>
+          <li><a href="#">news</a></li>
+          <li><a href="#">shop</a></li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
@@ -29,30 +31,34 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-.container {
-  width: 1000px;
-  min-height: 80px;
-  margin: 0 auto;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
+.container-fluid {
+  background: white;
 
-  .logo {
-    width: 60px;
-    height: 60px;
-  }
+  .container {
+    width: 1000px;
+    min-height: 80px;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
 
-  .nav-link {
-    ul {
-      display: flex;
-      gap: 15px;
+    .logo {
+      width: 60px;
+      height: 60px;
     }
 
-    a {
-      font-weight: 700;
-      text-decoration: none;
-      text-transform: uppercase;
-      color: currentColor;
+    .nav-link {
+      ul {
+        display: flex;
+        gap: 15px;
+      }
+
+      a {
+        font-weight: 700;
+        text-decoration: none;
+        text-transform: uppercase;
+        color: currentColor;
+      }
     }
   }
 }
