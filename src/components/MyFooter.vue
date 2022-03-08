0e06e3c47720a899e2e1035cf4ef8c159b4ed86c
Marco Corallo <94033528+marcocor25@users.noreply.github.com>
2022-03-08 15:04:03 +0100

    Caricamento assets e componenti

---
+++ src/components/MyFooter.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Test Footer</h1>
+    <img src="../assets/img/dc-logo.png" alt="" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyFooter",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
---
+++ src/components/MyHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Test Header</h1>
+    <img src="../assets/img/dc-logo.png" alt="" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyHeader",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
