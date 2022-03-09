d2b8fd0e027d128015edb0544654fdc07f9b4875
Marco Corallo <94033528+marcocor25@users.noreply.github.com>
2022-03-09 16:59:04 +0100

    Inserite thumbs e badge "current series"

---
+++ src/components/MyThumbs.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="card">
+    <figure class="card-image">
+      <img :src="img" :alt="title" />
+    </figure>
+    <h5 class="card-title">
+      {{ title }}
+    </h5>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyThumbs",
+  props: {
+    img: String,
+    title: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.card {
+  width: calc((100% / 6) - 20px);
+  height: 190px;
+  filter: drop-shadow(2px 2px 3px black);
+
+  .card-image {
+    aspect-ratio: 1/1;
+
+    img {
+      object-fit: cover;
+      object-position: top;
+      display: block;
+    }
+  }
+
+  .card-title {
+    padding: 10px 0;
+    text-transform: uppercase;
+  }
+}
+</style>
